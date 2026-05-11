555889a82c7c86b6b781c7b93b279457600038b1
ata2285591780 <ata2285591780@users.noreply.github.com>
2026-05-11 15:06:13 +0000

    Minimal working build

--- app/src/main/java/com/micinjector/hook/XposedEntry.java
+++ app/src/main/java/com/micinjector/hook/XposedEntry.java
@@ -1,46 +1,10 @@
 package com.micinjector.hook;
 
-import android.content.Context;
-import android.util.Log;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedEntry implements IXposedHookLoadPackage {
-    
-    private static final String TAG = "MicInjector";
-    
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals("com.micinjector")) {
-            Log.i(TAG, "MicInjector module loaded");
-            
-            XposedHelpers.findAndHookMethod(
-                "com.micinjector.hook.MainHook",
-                lpparam.classLoader,
-                "init",
-                XC_LoadPackage.LoadPackageParam.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        Log.i(TAG, "MainHook initialized");
-                    }
-                }
-            );
-        }
-        
-        try {
-            MainHook mainHook = MainHook.getInstance();
-            mainHook.init(lpparam);
-            
-            Log.i(TAG, "Hooked package: " + lpparam.packageName);
-            
-        } catch (Exception e) {
-            Log.e(TAG, "Error hooking package: " + lpparam.packageName);
-            Log.e(TAG, "Error: " + e.getMessage());
-        }
     }
 }
