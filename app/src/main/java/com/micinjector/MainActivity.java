019c89f7287cb4664d51eeed03bc4fa451b274fb
ata2285591780 <ata2285591780@users.noreply.github.com>
2026-05-11 15:11:05 +0000

    Absolute minimal build

--- app/src/main/java/com/micinjector/MainActivity.java
+++ app/src/main/java/com/micinjector/MainActivity.java
@@ -1,16 +1,15 @@
 package com.micinjector;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView tv = new TextView(this);
         tv.setText("Mic Injector");
-        tv.setPadding(48, 48, 48, 48);
         setContentView(tv);
     }
 }
